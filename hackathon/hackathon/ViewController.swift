8eb802eef76bef0c14faf494d906edcdfe1ba682
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-20 09:40:53 -0700

    first commit

---
+++ hackathon/hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackathon
+//
+//  Created by jeffrey dinh on 4/20/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
