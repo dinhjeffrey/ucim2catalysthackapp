e8d63ca4a81495aa293816c16a5750e35a19512d
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-23 12:53:59 -0700

    updated login with username, password, and debug text

--- hackathon/hackathon/ViewController.swift
+++ hackathon/hackathon/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func Login(sender: AnyObject) {
+        if usernameTextField.text!.isEmpty && passwordTextField.text!.isEmpty {
+            debugTextLabel.text = "Username and Password required."
+        } else if passwordTextField.text!.isEmpty {
+            debugTextLabel.text = "Password required."
+        } else if usernameTextField.text!.isEmpty {
+            debugTextLabel.text = "Username required."
+        } else {
+            // create a session here
+            debugTextLabel.text = "Login successful!"
+        }
+    }
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var debugTextLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
