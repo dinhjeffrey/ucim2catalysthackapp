91e01c853f5928bd296181292df059d198b6b4b3
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-30 20:57:55 -0700

    added redis, app.js, pug

---
+++ uci-backend/app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+var pug = require('pug')
+var redis = require('redis')
+var client = redis.createClient() //creates a new client
+
+// HTML render. Pug was formerly Jade
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+/* REDIS Defaults
+ port = 127.0.0.1
+ host = 6379
+*/
+
+var client = redis.createClient()
+
+client.on('connect', function() {
+    console.log('connected to Redis DB!')
+});
+
+app.listen(1714)
