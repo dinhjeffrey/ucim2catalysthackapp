7cfee0750a3676e10e540e378ba7d495033130b6
Charles Weng <cweng8849@gmail.com>
2016-05-13 13:31:56 -0700

    finished adding Device model

--- uci-sails/api/models/Device.js
+++ uci-sails/api/models/Device.js
@@ -1,7 +1,30 @@
 /**
  * Device.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * Useful Attributes:
+ *
+ * device_id
+ * company_id
+ * device_uuid
+ * device_type_id
+ * device_type
+ * device_os
+ * home_timezone
+ * device_name
+ * carrier_name
+ * language
+ * latest_post
+ * application_hash
+ * api_key
+ * create_date
+ * cpu_info
+ * cpu_max_speed
+ * cpu_cor_labels
+ * blocked
+ * blocked_reason
+ * unblock_date
+ *
+ * @description :: Data on each device
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
@@ -11,14 +34,26 @@ module.exports = {
     device_id: {
       type: 'integer',
       required: true,
-      unique: true
+      unique: true,
+      primaryKey: true
     },
     parent_device_id: {
       type: 'integer',
       required: false,
       unique: false
     },
-
+    /*UNUSED*/
+    m2_id: {
+      type: 'integer',
+      required: false,
+      unique: false
+    },
+    user_id: {
+      type: 'integer',
+      required: false,
+      unique: false
+    },
+    /*UNUSED*/
     company_id: {
       type: 'integer',
       required: false,
@@ -30,27 +65,133 @@ module.exports = {
       unique: true
     },
     device_type_id: {
-      type: ''
+      type: 'integer',
+      required: false,
+      unique: false
     },
     device_type: {
-
+      type: 'string',
+      required: false,
+      unique: false
     },
     device_os: {
-
+      type: 'string',
+      required: false,
+      unique: false
     },
     home_timezone: {
-
+      type: 'integer',
+      required: false,
+      unique: false
     },
-
-
-
-
-    /*UNUSED*/
-    m2_id: {
-
+    device_name: {
+      type: 'string',
+      required: false,
+      unique: false
     },
-    user_id: {
-
+    carrier_name: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    mcc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    mnc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    n_mcc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    n_mnc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    s_mcc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    s_mnc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    r_mcc: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    language: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    latest_post: {
+      type: 'text',
+      required: false,
+      unique: false
+    },
+    device_secret: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    application_hash: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    api_key: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    create_date: {
+      type: 'datetime',
+      required: false,
+      unique: false
+    },
+    cpu_info: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    cpu_max_speed: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    cpu_core_labels: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    blocked: {
+      type: 'boolean',
+      required: false,
+      unique: false
+    },
+    blocked_reason: {
+      type: 'text',
+      required: false,
+      unique: false
+    },
+    unblock_date: {
+      type: 'datetime',
+      required: false,
+      unique: false
+    },
+    toJSON: function() {
+      var device = this.toJSON();
+      return device;
     }
   }
 };
