9a9f79b5793b77d8fbba3dc66a80775bb745c880
Charles Weng <cweng8849@gmail.com>
2016-05-13 20:19:26 -0700

    add m2tables & moved *.tables into that folder

--- rscripts/exploredevicedata.R
+++ rscripts/exploredevicedata.R
@@ -1,1 +1,14 @@
 devices <- read.csv(file = '../M2C Sample/devices.csv', header = F)
+##dealing with null device values##
+deviceid[deviceid=='NULL']
+##deviceid from fact to int##
+deviceid <- as.numeric(deviceid)
+##checking duplicate values##
+as.numeric(deviceid)[!duplicated(as.numeric(deviceid))]
+as.numeric(deviceid)[duplicated(as.numeric(deviceid))]
+##indicates samung, LG, Nexus,HTC etc##
+##split this into model & device##
+devicename <- devices$V8
+
+
+
