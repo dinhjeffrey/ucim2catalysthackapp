daad722736fe686983d881b19c452fd891d13642
Charles Weng <cweng8849@gmail.com>
2016-05-13 21:24:38 -0700

    add setwd and getwd and listfiles commands for R

--- rscripts/exploreappdata.R
+++ rscripts/exploreappdata.R
@@ -1,3 +1,7 @@
+#simple file 
+setwd('~/Documents/uci-hackathon-app/rscripts/')
+list.files('.')
+getwd()
 apps <- read.csv(file = '../M2C Sample/application_versions.csv', header=F, nrow=10000)
 str(apps)
 #rename columns
