b2893ca1d8ebeb1ca3bbd3f8830983d65ec75e8b
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-05-14 17:40:09 -0700

    deleted d3 files and m2tables

--- d3/8-d3js-data-vis-fundamentals-exercise-files/functions.js
+++
@@ -1,9 +0,0 @@
-			function formatSales(d){
-    			var prefix = d3.formatPrefix(d);
-    			var num =  prefix.scale(d).toFixed();						    			
-    			return num+prefix.symbol;
-			}
-
-			function compareNums(a,b){
-				return a-b;
-			}
