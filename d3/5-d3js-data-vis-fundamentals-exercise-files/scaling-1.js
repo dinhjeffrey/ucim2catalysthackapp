986b0a666d8c138701e06d7927e63285659b8631
Charles Weng <cweng8849@gmail.com>
2016-05-14 19:24:56 -0700

    deleting d3 again

--- d3/5-d3js-data-vis-fundamentals-exercise-files/scaling-1.js
+++
@@ -1,8 +0,0 @@
-var scale = d3.scale
-              .linear()
-              .domain([130,350]) //input min/max of sales values
-              .range([10,100]); //output pixels for SVG
-
-console.log(scale(300)); //output 79.54
-console.log(scale(270)); //output 67.27
-console.log(scale(150)); //output 18.18
